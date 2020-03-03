a5ee87521ad304269bc94f29831679f038ffd80c
samrudhi90 <samrudhi90@gmail.com>
2020-03-03 12:19:17 +0530

    Update views.py

--- Serveradmin/Serveradmin/views.py
+++ Serveradmin/Serveradmin/views.py
@@ -38,6 +38,38 @@ def external(request):
       tn.read_until("Password: ")
       tn.write(password + "\n")
 
-   print ("Capturing and printing pre-upgrade logs. Please wait......")
+  print ("Capturing and printing pre-upgrade logs. Please wait......")
+# Below commands are to capture pre outputs!! 
+  tn.write("enable \n")
+  tn.write("ter len 0 \n")
+  tn.write("show version \n")
+  tn.write("show bootvar \n")
+  tn.write("show run \n")
+  time.sleep(15)
+           
+  tn.write("\n")
+
+# Below is to configure parameters and to exit:
+
+  print ("Starting to upgrade now")
+
+# Below syntax is to start upgrade
+
+  tn.write("conf t\n")
+  tn.write("no boot system \n")
+  tn.write("boot system flash bootflash:")
+  tn.write(Image_name)
+  tn.write("\n")
+  tn.write("exit \n")
+  tn.write("write memory \n")
+  tn.write("show bootvar \n")
+  tn.write("reload \n")
+  time.sleep(1)
+
+  tn.read_until("Proceed with reload?")
+
+  tn.write("yes \n")
+
+  print tn.read_all()
    
-   return render(request,'home.html',{'data1':data1}) 
+  return render(request,'home.html',{'data1':data1}) 
