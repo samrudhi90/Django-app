d08ce8310fd53ee290850f262b4511526b9b7b74
Samrudhi Sheth Morajkar <smoraj27@in.ibm.com>
2020-03-05 03:31:30 +0530

    Working CSS

--- Serveradmin/Serveradmin/views.py
+++ Serveradmin/Serveradmin/views.py
@@ -6,7 +6,7 @@ import sys
 import telnetlib
 import time
 import paramiko
-
+from . import views
 
 from subprocess import run,PIPE
 data= ""
@@ -15,16 +15,10 @@ def button(request):
     return render(request,'home.html')
 
 def output(request):
-
-   hostname= request.POST.get('param1')
-   
-   response = os.system("ping -c 1 " + hostname)
-
-   if response == 0:
-       data=hostname + ", is up!"
-       
-   else:
-       data=hostname + ", is down!"
+   data=request.session['data'] +"\n"+ request.session['IP']
+   data= data +"\n"+ request.session['username'] 
+   image=request.POST.get('dldImage')
+   data= data+"\n"+ image
    return render(request,'home.html',{'data':data}) 
 
 def external(request):
@@ -32,8 +26,17 @@ def external(request):
    hostname= request.POST.get('param1')
    username = request.POST.get('param2')
    password = request.POST.get('param3')
+   request.session['IP'] = hostname
+   request.session['username'] = username
+   response = os.system("ping -c 1 " + hostname)
 
- 
+   if response == 0:
+       data=hostname + ", is up!"
+       
+   else:
+       data=hostname + ", is down!"
+       request.session['data'] = data
+#  return render(request,'home.html',{'data':data.strip()}) 
 
 #
 # save host login info
@@ -43,77 +46,43 @@ def external(request):
 # We first set up ssh session.
 #
 
-remote_conn_pre = paramiko.SSHClient()
-remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-remote_conn_pre.connect(hostname, port=22, username=username, password=password, look_for_keys=False, allow_agent=False)
-print("SSH connection established to " + hostname)
-remote_conn = remote_conn_pre.invoke_shell()
+#   remote_conn_pre = paramiko.SSHClient()
+#   remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#   remote_conn_pre.connect(hostname, port=22, username=username, password=password, look_for_keys=False, allow_agent=False)
+#   print("SSH connection established to " + hostname)
+#   remote_conn = remote_conn_pre.invoke_shell()
 
 #
 # extract show version output
 # Disable pagination in the termial
 #
-remote_conn.send("terminal length 0\n")
-remote_conn.send("\n")
-remote_conn.send("show version\n")
-remote_conn.send("\n")
-time.sleep(5)
-if remote_conn.recv_ready():
-        show_version = remote_conn.recv(1000)
+#   remote_conn.send("terminal length 0\n")
+#   remote_conn.send("\n")
+#   remote_conn.send("show version\n")
+#   remote_conn.send("\n")
+#   time.sleep(5)
+#   if remote_conn.recv_ready():
+#         show_version = remote_conn.recv(1000)
 #
 # To print the CLI "output" as seen in the CLI it needs to
 # be formatted in String format "str" "utf-8" as shown below
 #
-show_version_str = show_version.decode('utf-8')
-platform_version_lst = platform_version_lst
+#   show_version_str = show_version.decode('utf-8')
+#   platform_version_lst = platform_version_lst
 #
 # Extract version information
 #
 
+#   for platform_version in show_version_str.splitlines():
+#       if 'Cisco IOS Software' in platform_version:
+#          print (platform_version)
+#   data=data + platform_version_lst = platform_version
+#   platform_version_lst = platform_version_lst.split()
 
-for platform_version in show_version_str.splitlines():
-    if 'Cisco IOS Software' in platform_version:
-        print (platform_version)
-platform_version_lst = platform_version
-platform_version_lst = platform_version_lst.split()
-print ( platform_version_lst )	
+#   print ( platform_version_lst )
+    
+#   remote_conn.send("\n")
 #
-remote_conn.send("\n")
 #
-#
-  print ("Capturing and printing pre-upgrade logs. Please wait......")
-# Below commands are to capture pre outputs!! 
-  tn.write("enable \n")
-  tn.write("ter len 0 \n")
-  tn.write("show version \n")
-  tn.write("show bootvar \n")
-  tn.write("show run \n")
-  time.sleep(15)
-           
-  tn.write("\n")
-
-# Below is to configure parameters and to exit:
-
-  print ("Starting to upgrade now")
-
-# Below syntax is to start upgrade
-
-  tn.write("conf t\n")
-  tn.write("no boot system \n")
-  tn.write("boot system flash bootflash:")
-  tn.write(Image_name)
-  tn.write("\n")
-  tn.write("exit \n")
-  tn.write("write memory \n")
-  tn.write("show bootvar \n")
-  tn.write("reload \n")
-  time.sleep(1)
-
-  tn.read_until("Proceed with reload?")
-
-  tn.write("yes \n")
-
-  print tn.read_all()
-
    
-  return render(request,'home.html',{'data1':data1}) 
+   return render(request,'home.html',{'data':data}) 
